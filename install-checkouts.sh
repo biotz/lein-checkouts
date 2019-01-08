60213494f18134ede05e5abb10e23a8210ede00d
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-01-08 10:16:48 +0100

    Fix shell linter warnings.

--- install-checkouts.sh
+++ install-checkouts.sh
@@ -11,8 +11,8 @@ find "${CHECKOUTS_SOURCES}" -type f -name "project.clj" -print0 |
     while IFS= read -r -d '' MODULE_DIR; do
         MODULE_DIR=$(dirname "${MODULE_DIR}")
         echo "Installing and linking module from ${MODULE_DIR}"
-        pushd "${MODULE_DIR}" > /dev/null
+        pushd "${MODULE_DIR}" >/dev/null
         lein install
-        popd > /dev/null
+        popd >/dev/null
         ln -s ../"${MODULE_DIR}" checkouts/
     done
